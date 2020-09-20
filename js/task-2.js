352f3d7687bb677949f8e7bdb080c53fef6fde7f
Reznik Denis <63472391+reznik-denis@users.noreply.github.com>
2020-09-20 09:38:27 +0300

    добавляет дз 2

---
+++ js/task-2.js
@@ -0,0 +1,12 @@
+const ingredients = [
+    'Картошка',
+    'Грибы',
+    'Чеснок',
+    'Помидоры',
+    'Зелень',
+    'Приправы',
+];
+
+const listCreateEl = document.querySelector('#ingredients');
+
+ingredients.
