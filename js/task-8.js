209d4c3bbc5af5539e6523d4545dfa0852d83761
Reznik Denis <63472391+reznik-denis@users.noreply.github.com>
2020-09-22 14:31:28 +0300

    додає перевірку на 8 задачі

--- js/task-8.js
+++ js/task-8.js
@@ -6,6 +6,14 @@ const refs = {
 }
 
 function createBoxes(amount) {
+    if (amount > 100) {
+        refs.input.classList.remove('valid');
+        refs.input.classList.add('invalid');
+        return
+    } else {
+        refs.input.classList.remove('invalid');
+        refs.input.classList.add('valid');
+    }
     const createCollection = [];
     for (let i = 0; i < amount; i += 1) {
         const newEl = `<div style="width: ${30 + i * 10}px; height: ${30 + i * 10}px; background-color: rgb(${Math.random() * (255 - 1) + 1}, ${Math.random() * (255 - 1) + 1}, ${Math.random() * (255 - 1) + 1});"></div>`;
